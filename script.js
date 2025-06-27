0749ee49367403f6e787ea2454a44e70cc559deb
vitor184 <vitor.moreira.silva02@escola.pr.gov.br>
2025-06-26 21:36:31 -0300

    aula1 -5

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
-    alert("Fui clicado!");
+    //alert("Fui clicado!");
+    const texto = Document.querySelector('#entrada-de-texto').value;
+    const campoResultado = Document.querySelector('resultado-palavrachave')
+    const palavras = texto.split('')
+
+    campoResultado.textContent = palavras.join(", ")
 }
