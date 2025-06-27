991582e50597106ca6958cea275d5a7439a12665
vitor184 <vitor.moreira.silva02@escola.pr.gov.br>
2025-06-26 22:05:50 -0300

    aula 3 - 26/06

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ function mostraPalavraChave() {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ")
+    const palavras = processaTesto(testo)
 
     campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTesto(testo) {
+    //let palavras = testo.split(/\s+/);  "retirar espaços"
+    //let palavras = testo.split(/[a-zA-Z]+/); "letras minusculas e maiusculas"
+    let palavras = testo.split("/\P{L}+/u/");  /* \P negação; {L} conjunto de letras; + uma ou mais ocorrencias; u Unidade 
+
+    let frequencias = []
+
+    for (let i in palavras) {
+    frequencia[i] = 0;
+    
+    }
+
+    console.log(frequencia);
+
+    return palavras;
 }
