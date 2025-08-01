bcb0dae5435c50a1b14872279765632b18273f02
vitor184 <vitor.moreira.silva02@escola.pr.gov.br>
2025-07-31 21:07:59 -0300

    aula 8 31-07

--- script.js
+++ script.js
@@ -8,15 +8,22 @@ function mostraPalavraChave() {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
     //let palavras = texto.split(/\s+/);  "retirar espaços"
     //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
     let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i]. toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras)
+        
 
     const frequencias = contaFrequencias(palavras);
 
